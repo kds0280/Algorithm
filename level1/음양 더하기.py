a2c797b16609862a19ee73acffa2ca76bc14c312
kds0280 <kds0280@naver.com>
2021-06-25 07:41:03 +0900

    level1

---
+++ level1/음양 더하기.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(signs)):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+    return answer
+
