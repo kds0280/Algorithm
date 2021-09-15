7d38057bc52ce2903d0a9ad8a70d257d691afd73
Hazzzero <58512335+kds0280@users.noreply.github.com>
2021-09-15 14:19:38 +0900

    fail: I misunderstood the intention

---
+++ level2/124나라의 숫자.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def solution(n):
+    answer = ''
+    while n > 0:
+        n -= 1
+        answer = '124'[n%3] + answer
+        n //= 3
+    return answer
+
